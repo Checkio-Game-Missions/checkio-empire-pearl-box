91266b8f22acfcdc16f0c9b321194b67fb947247
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-11-17 18:26:09 +1200

    tests

--- verification/tests.py
+++ verification/tests.py
@@ -7,30 +7,110 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
-        {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
-        },
-        {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+        {"input": ('bbw', 3),
+         "answer": 0.481,
+         "explanation": 0.48},
+
+        {"input": ('wwb', 3),
+         "answer": 0.519,
+         "explanation": 0.52},
+
+        {"input": ('www', 3),
+         "answer": 0.556,
+         "explanation": 0.56},
+
+        {"input": ('bbbb', 1),
+         "answer": 0.0,
+         "explanation": 0.0},
+
+        {"input": ('wwbb', 4),
+         "answer": 0.5,
+         "explanation": 0.5},
+
+        {"input": ('bwbwbwb', 5),
+         "answer": 0.481,
+         "explanation": 0.48},
     ],
     "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+        {"input": ('bwwbbbbw', 4),
+         "answer": 0.447,
+         "explanation": 0.45},
+
+        {"input": ('bbb', 5),
+         "answer": 0.494,
+         "explanation": 0.49},
+
+        {"input": ('bwbwbb', 4),
+         "answer": 0.451,
+         "explanation": 0.45},
+
+        {"input": ('wbbb', 3),
+         "answer": 0.438,
+         "explanation": 0.44},
+
+        {"input": ('wwbbwbwbwb', 2),
+         "answer": 0.5,
+         "explanation": 0.5},
+
+        {"input": ('wbwwwww', 2),
+         "answer": 0.755,
+         "explanation": 0.76},
+
+        {"input": ('wbwwwwwwwb', 4),
+         "answer": 0.654,
+         "explanation": 0.65},
+
+        {"input": ('bwb', 2),
+         "answer": 0.444,
+         "explanation": 0.44},
+
+        {"input": ('bbbbw', 5),
+         "answer": 0.461,
+         "explanation": 0.46},
+
+        {"input": ('wbwwwwwb', 3),
+         "answer": 0.641,
+         "explanation": 0.64},
+    ],
+    "3. Weird": [
+        {"input": ('w', 1),
+         "answer": 1.0,
+         "explanation": 1.0},
+
+        {"input": ('b', 1),
+         "answer": 0.0,
+         "explanation": 0.0},
+
+        {"input": ('w', 2),
+         "answer": 0.0,
+         "explanation": 0.0},
+
+        {"input": ('b', 2),
+         "answer": 1.0,
+         "explanation": 1.0},
+
+        {"input": ('wwwwwwwwwwww', 4),
+         "answer": 0.789,
+         "explanation": 0.79},
+
+        {"input": ('bbbbbbbbbbbb', 5),
+         "answer": 0.259,
+         "explanation": 0.26},
+    ],
+    "4. Extra": [
+        {"input": ('wwwwwbwbwbbwbbwwbw', 11),
+         "answer": 0.534,
+         "explanation": 0.53},
+
+        {"input": ('wwbwwwbbwbbwww', 10),
+         "answer": 0.536,
+         "explanation": 0.54},
+
+        {"input": ('bbwbwbbwbwwbw', 11),
+         "answer": 0.493,
+         "explanation": 0.49},
     ]
+
 }
